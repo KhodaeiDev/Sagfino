034c59739674e375adc0a2a96668988cb213e5aa
arwinGhaderi <arwinghaderi81@gmail.com>
2025-02-26 19:53:11 +0330

    Completing the real estate news section.

---
+++ front-end/src/components/newsBox/newsBox.tsx
@@ -0,0 +1,20 @@
+const NewsBox = () => {
+  return (
+    <>
+      <div className=" bg-boxHelp  border  border-gray-D9  rounded-lg">
+        <img
+          className="rounded-t-lg w-full"
+          src="../../../public/img/Rectangle 52.png"
+          alt=""
+        />
+        <div className="   py-4  px-4 lg:py-7 lg:px-4">
+          <h5 className=" font-shabnamBold text-xs lg:text-xl text-black">
+            خطر ویرانی زلزله در آسمان‌خراش‌ها بیشتر است یا...
+          </h5>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default NewsBox
