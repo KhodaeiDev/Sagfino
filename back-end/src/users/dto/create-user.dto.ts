70135845c8c8db47f2a56a4a7a81b8bd7cf3cf16
khodaeidev <khodaei.dev@gmail.com>
2025-03-05 15:48:32 +0330

    Dev user Dto

--- back-end/src/users/dto/create-user.dto.ts
+++ back-end/src/users/dto/create-user.dto.ts
@@ -1,1 +1,44 @@
-export class CreateUserDto {}
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+  Matches,
+  MinLength,
+} from 'class-validator';
+import userRoleEnum from '../enum/userRoleEnum';
+import { Transform } from 'class-transformer';
+
+export class CreateUserDto {
+  @IsString()
+  @Length(11, 11, { message: 'شماره تلفن باید 11 رقم باشد.' })
+  @Matches(/^09\d{9}$/, {
+    message: 'لطفا شماره تلفن را در فرمت صحیح وارد کنید.',
+  })
+  @Transform(({ value }) => value.trim())
+  @IsNotEmpty({ message: 'شماره موبایل الزامی می باشد' })
+  phone: string;
+
+  @IsString()
+  @MinLength(3, { message: 'نام باید حداقل دارای 3 کاراکتر باشد' })
+  @IsNotEmpty({ message: 'نام الزامی می باشد' })
+  firstName: string;
+
+  @IsString()
+  @MinLength(3, { message: 'نام خانوادگی باید حداقل دارای 3 کاراکتر باشد' })
+  @IsNotEmpty({ message: 'نام خانوادگی الزامی می باشد' })
+  lastName: string;
+
+  @IsString()
+  @MinLength(8, { message: 'رمز عبور باید حداقل دارای 8 کاراکتر باشد' })
+  password: string;
+
+  @IsString()
+  @IsOptional()
+  avatar: string;
+
+  @IsEnum(userRoleEnum)
+  @IsOptional()
+  role: userRoleEnum;
+}
