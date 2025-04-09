7a161d4d64aa91aed07a7b50fa5e71085133abe0
arwinGhaderi <arwinghaderi81@gmail.com>
2025-04-09 10:45:06 +0330

    feat: Adding .vercel.json to enable 404 routing

--- front-end/src/routes/routes.tsx
+++ front-end/src/routes/routes.tsx
@@ -1,23 +1,22 @@
+// imports
 import NotFound from '../pages/404/NotFound'
 import SiteApp from '../pages/site/siteApp/siteApp'
+import RegisterAnAdApp from '../pages/registerAnAd/RegisterAnAdApp'
+import CmsApp from '../pages/cms/cmsApp'
 import siteRoutes from './siteRoutes'
 import RegisterAnAdRoutes from './registerAnAdRoutes'
-import RegisterAnAdApp from '../pages/registerAnAd/RegisterAnAdApp'
 import CmsRoutes from './cmsRoutes'
-import CmsApp from '../pages/cms/cmsApp'
 
-const routes = [
+const baseRoutes = [
   { path: '/', element: <SiteApp />, children: [...siteRoutes] },
-
   { path: '/cms', element: <CmsApp />, children: [...CmsRoutes] },
-
   {
     path: '/registerAnAd',
     element: <RegisterAnAdApp />,
     children: [...RegisterAnAdRoutes],
   },
-
-  { path: '*', element: <NotFound /> },
 ]
+
+const routes = [...baseRoutes, { path: '*', element: <NotFound /> }]
 
 export default routes
